535847b85f9e486c8c2685fdce2d038d23f2c3fe
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2024-04-07 00:40:20 +0530

    dobly linked list completed

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -18,7 +18,7 @@ class DoublyLinkedList:
         else:
             print("None")
 
-    def displayListBack(self) -> None:
+    def displayListReverse(self) -> None:
         if self.head:
             temp = self.head
             while self.head.next != None:
@@ -30,7 +30,7 @@ class DoublyLinkedList:
         else:
             print("None")
 
-    def insertFirst(self, value) -> object:
+    def insertFirst(self, value: any) -> None:
         if self.head:
             newNode = Node(value)
             newNode.next = self.head
@@ -40,7 +40,7 @@ class DoublyLinkedList:
             newNode = Node(value)
             self.head = newNode
 
-    def insertEnd(self, value) -> object:
+    def insertEnd(self, value: any) -> None:
         if self.head:
             temp = self.head
             while self.head.next != None:
@@ -53,7 +53,7 @@ class DoublyLinkedList:
             newNode = Node(value)
             self.head = newNode
 
-    def deleteEnd(self) -> object:
+    def deleteEnd(self) -> None:
         if self.head:
             temp = self.head
             while self.head.next != None:
@@ -64,16 +64,76 @@ class DoublyLinkedList:
         else:
             pass
 
+    def deleteFirst(self) -> None:
+        if self.head:
+            temp = self.head
+            self.head = temp.next
+            temp = None
+        else:
+            print("Empty List")
+
+    def insertBetween(self, index: int, value: any) -> None:
+        if self.head:
+            temp = self.head
+            for _ in range(index-1):
+                if self.head:
+                    self.head = self.head.next
+                else:
+                    print("Index out of range")
+                    return
+            newElement = Node(value, self.head, self.head.next)
+            self.head.next = newElement
+            self.head = temp
+        else:
+            print("Empty List")
+
+    def deleteBetween(self, index: int) -> None:
+        if self.head:
+            temp1 = self.head
+            for _ in range(index-1):
+                if self.head:
+                    self.head = self.head.next
+                else:
+                    print("Index out of range")
+                    return
+            temp = self.head.next
+            if temp != None:
+                self.head.next = temp.next
+            else:
+                self.head = temp1
+                print("Reached to the end")
+                return
+            temp.prev = self.head
+            self.head = temp1
+        else:
+            print("Empty List")
+
+
+
+
 
 link = DoublyLinkedList()
 link.insertFirst(5)
 link.insertFirst(8)
+link.insertFirst(19)
+link.insertFirst('Alok')
+link.insertFirst('Python')
+link.insertFirst('Uvicorn')
 link.insertEnd(9)
 print("Print List: ")
 link.displayList()
-print("Print List Backward: ")
-link.displayListBack()
+print("Print List Reversed: ")
+link.displayListReverse()
+print("Delete end: ")
 link.deleteEnd()
-print("Print List: ")
+link.displayList()
+print("Delete first: ")
+link.deleteFirst()
+link.displayList()
+print("Insert between: ")
+link.insertBetween(1,'ASGI')
+link.displayList()
+print("Delete between: ")
+link.deleteBetween(3)
 link.displayList()
 
